cf42d902ece3ba13f08dd99b095550979362c8ce
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-28 08:37:36 +0900

    Initial Commit

---
+++ APIKitCombineSample/APIKitCombineSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIKitCombineSampleApp.swift
+//  APIKitCombineSample
+//
+//  Created by Yusuke Hasegawa on 2021/06/28.
+//
+
+import SwiftUI
+
+@main
+struct APIKitCombineSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
